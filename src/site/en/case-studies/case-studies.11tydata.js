10ca37b3e880db8e982ba1a05dbd792b2b36cc3b
Tom Vaughan <thomas.david.vaughan@gmail.com>
2026-02-18 21:12:28 -0300

    Add case studies.

---
+++ src/site/en/case-studies/case-studies.11tydata.js
@@ -0,0 +1,10 @@
+export default {
+  layout: "post.njk",
+  tags: ["posts"],
+  author: {
+    avatar: "webmaster.html",
+    name: "Nalca Biotech",
+    email: "webmaster@nalca.bio",
+    title: "Webmaster",
+  },
+};
